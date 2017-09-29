c754328eb901903b265fdb0f7d71de89dea39d91
aquac <aquac@aquac.com>
2017-09-29 15:32:52 +0200

    use pan's get methods

--- providers/aseag.py
+++ providers/aseag.py
@@ -20,10 +20,10 @@ Public transport stops and departures from Aachen, Germany (Aseag).
 """
 
 import pan
-import requests
 import re
 import urllib.parse
 import json
+from operator import itemgetter
 
 baseurl		= "http://ivu.aseag.de/interfaces/ura/{}"
 url_l		= "location"
@@ -31,37 +31,24 @@ url_i		= "instant_V2"
 returnlist	= "StopPointName,StopID,StopPointState,StopPointIndicator,Latitude,Longitude,VisitNumber,TripID,VehicleID,LineID,LineName,DirectionID,DestinationName,DestinationText,EstimatedTime,BaseVersion"
 
 def find_departures(stops):
-	#print( "find_dep:"+str(stops[0]) )
-	parameter = {'ReturnList': returnlist, 'StopID': ','.join(map(str, stops)) }
-	request = requests.get(baseurl.format(url_i), params = parameter)
-	if request.status_code != 200:
-		raise Exception
-	if request.headers['content-type'] != 'application/json;charset=UTF-8':
-		raise Exception
-	#print(request.text)
-	data = parsejson_find_departures(request.text)
-	print(data)
-	#print("\n\n\n")
-	return data
+	parameter = {"ReturnList": returnlist, "StopID": ",".join(map(str, stops)) }
+	request = pan.http.get( format_url(baseurl.format(url_i), parameter), encoding="utf_8" )
+	data = parsejson_find_departures(request)
+	return sorted(data, key=itemgetter("time")) 
 
 def parsejson_find_departures(data):
 	output = []
 	for line in data.splitlines():
 		linelist = json.loads(line)
 		if (linelist[0] == 1):
-			output.append({ "destination": linelist[11], "line": linelist[9], "realtime": True, "scheduled_time": linelist[15], "stop": linelist[2], "time": linelist[15], "y": linelist[5], "x": linelist[6] })
+			output.append({ "destination": linelist[11], "line": linelist[9], "realtime": True, "scheduled_time": linelist[15]/1000, "stop": linelist[2], "time": linelist[15]/1000, "y": linelist[5], "x": linelist[6] })
 	return output
 
 def find_lines(stops):
-	"""Return a list of lines that use `stops`."""
-	parameter = {'ReturnList': returnlist, 'StopID': ','.join(map(str, stops)) }
-	request = requests.get(baseurl.format(url_i), params = parameter)
-	if request.status_code != 200:
-		raise Exception
-	if request.headers['content-type'] != 'application/json;charset=UTF-8':
-		raise Exception
-	data = parsejson_find_lines(request.text)
-	return data
+	parameter = {"ReturnList": returnlist, "StopID": ",".join(map(str, stops)) }
+	request = pan.http.get( format_url(baseurl.format(url_i), parameter), encoding="utf_8" )
+	data = parsejson_find_lines(request)
+	return sorted(data, key=itemgetter("name")) 
 
 def parsejson_find_lines(data):
 	output = []
@@ -75,21 +62,24 @@ def parsejson_find_lines(data):
 
 
 def find_nearby_stops(x, y):
-	return {"color": "#007ac9", "description": "", "id": "", "line_summary": "", "name": "", "x": 0, "y": 0}
+	return {}
 
 def find_stops(query, x, y):
-	parameter = {'searchString': query, 'maxResults': 10, 'searchTypes': 'STOPPOINT'}
-	request = requests.get(baseurl.format(url_l), params=parameter)
-	if request.status_code != 200:
-		raise Exception
-	if request.headers['content-type'] != 'application/json;charset=UTF-8':
-		raise Exception
-	data = parsejson_find_stops(request.json())
+	parameter = {"searchString": query, "maxResults": "10", "searchTypes": "STOPPOINT"}
+	request = pan.http.get_json( format_url(baseurl.format(url_l), parameter), encoding="utf_8" )
+	data = parsejson_find_stops(request)
 	return data
 
 def parsejson_find_stops(data):
 	output = []
-	data = data['resultList'];
+	data = data["resultList"]
 	for line in data:
-		output.append({ "color": "#007ac9", "description": line['stopPointName'], "id": line['stopPointId'], "line_summary": "",  "name": line['stopPointName'], 'x': line['longitude'], 'y': line['latitude'] })
+		output.append({ "color": "#007ac9", "description": line["stopPointName"], "id": line["stopPointId"], "line_summary": "",  "name": line["stopPointName"], "x": line["longitude"], "y": line["latitude"] })
 	return output
+
+
+def format_url(url, p, **params):
+	"""Return API URL for `path` with `params`."""
+	params.update(p)
+	params = "&".join("=".join(x) for x in params.items())
+	return "?".join((url, params))
